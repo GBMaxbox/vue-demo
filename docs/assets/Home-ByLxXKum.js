ebc955ce60b715bacdbbc26b7dfca22b648f3ee7
Wberl <1903008436@qq.com>
2025-03-28 11:51:54 +0800

    test 1

---
+++ docs/assets/Home-ByLxXKum.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index-DP58MSqj.js";const a={__name:"Home",setup(c){return console.log("来到了home"),(t,n)=>(e(),o("p",null,"我是Home组件"))}};export{a as default};
---
+++ docs/assets/student-BfOiaGMk.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-DP58MSqj.js";const n={};function o(r,s){return t(),c("h3",null,"我是学生组件")}const a=e(n,[["render",o]]);export{a as default};
